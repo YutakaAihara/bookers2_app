e75f88e3f5812c7a4a159579f65cda5abb148ce3
YutakaAihara <youguisufanyuan@gmail.com>
2021-04-26 09:45:59 +0000

    [Update]booksのindex.showのレイアウト・機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,38 @@
 class BooksController < ApplicationController
 
   def index
-  end 
-  
+    @books = Book.all
+    @book = Book.new
+  end
+
   def create
-  end 
-  
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
   def show
-  end 
-  
+    @book = Book.new
+    @show_book = Book.find(params[:id])
+    @user = @show_book.user
+
+  end
+
   def edit
-  end 
-  
+  end
+
   def update
-  end 
-  
+  end
+
   def destroy
-  end 
-  
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
-  end 
-  
-  
+  end
+
+
 end
