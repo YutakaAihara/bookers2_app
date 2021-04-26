25a043e171c89e0347144bdd0f7bf4424cee3dff
YutakaAihara <youguisufanyuan@gmail.com>
2021-04-26 10:00:13 +0000

    [Update]userのindexレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
-  
+
   def index
+    @users = User.all
+    @book = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def edit
-  end 
-  
+  end
+
   def update
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile, :image)
   end
